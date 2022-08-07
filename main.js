303168bc206f9779d115515905d857665a442559
Mohammad Ali <ali.shiekh16@outlook.com>
2022-08-07 15:43:42 +0500

    Add orbit controls.

--- main.js
+++ main.js
@@ -4,16 +4,23 @@ import { renderer, updateRenderer } from './renderer';
 import { scene } from './scene';
 import { camera, updateCamera } from './camera';
 import { configs } from './configurations';
+import { createGalaxy } from './galaxy';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 const { sizes, renderer: rendererConfigs } = configs;
 
-const geometry = new THREE.SphereBufferGeometry(1, 16, 16);
-const material = new THREE.MeshBasicMaterial();
-const sphere = new THREE.Mesh(geometry, material);
-
-scene.add(sphere);
+createGalaxy();
 
 renderer.render(scene, camera);
+
+const controls = new OrbitControls(camera, renderer.domElement);
+
+function animate() {
+  controls.update();
+  requestAnimationFrame(animate);
+}
+
+animate();
 
 window.addEventListener('resize', () => {
   sizes.width = window.innerWidth;
